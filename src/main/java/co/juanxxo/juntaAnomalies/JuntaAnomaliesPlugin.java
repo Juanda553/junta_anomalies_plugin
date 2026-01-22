92e82023cfaffce52561724bff49d9e25760098f
Juan Zapata <juan.zapata.199@gmail.com>
2026-01-22 01:04:12 -0500

    Feature: Registro de anomalias secilla y Anomalia: ZombieOfDeadPlayer

--- src/main/java/co/juanxxo/juntaAnomalies/JuntaAnomaliesPlugin.java
+++ src/main/java/co/juanxxo/juntaAnomalies/JuntaAnomaliesPlugin.java
@@ -1,19 +1,30 @@
 package co.juanxxo.juntaAnomalies;
 
 import co.juanxxo.juntaAnomalies.anomalies.AnomaliesListener;
-import org.bukkit.Bukkit;
+import co.juanxxo.juntaAnomalies.anomalies.ZombieOfDeathPlayer;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JuntaAnomaliesPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new AnomaliesListener(), this);
+        // REGISTRAR ANOMALÍAS
+        // USAR anomaly(new ClaseDeLaAnomalia()) PARA REGISTRAR CADA ANOMALÍA
+
+        anomaly(new AnomaliesListener());
+        anomaly(new ZombieOfDeathPlayer());
+
+        ////////////////////////////////////////////
         getLogger().info("Anomalías activadas");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("Anomalías desactivadas");
+    }
+
+    private void anomaly(Listener listener) {
+        getServer().getPluginManager().registerEvents(listener, this);
     }
 }
